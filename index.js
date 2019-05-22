b4f75a0f3cf407a8262d1aaad8e8bd7d25c4a755
EganJ <EganJ@users.noreply.github.com>
2019-05-22 15:28:43 -0500

    Query by zip

--- index.js
+++ index.js
@@ -2,17 +2,50 @@ const express = require('express');
 const path = require('path');
 const census = require('citysdk');
 const request = require('request');
+const zipcodes = require("zipcodes");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
   res.render('pages/index');
 });
 app.post('/', function (req, res) {
-  
+  var zipdata = zipcodes.lookup(req.body.area);
+  if (!zipdata) {
+    //Cannot find zip code.
+    //TODO: return error message.
+    return;
+  }
+  //get school district
+  var zipcode;
+  var block = true;
+  census({
+    "vintage": 2017,
+    "geoHierarchy": {
+      "zip-code-tabulation-area": {
+        "lat": zipdata.latitude,
+        "lng": zipdata.longitude
+      }
+    }
+  }, function (err, censusres) {
+    if (!err == null) {
+      zipcode = -1;
+      console.log("<!-Error:", err);
+      console.log("-!>");
+    } else {
+      zipcode = censusres.geoHierarchy["zip code tabulation area"];
+      getPage(zipcode, res);
+    }
+  });
+});
+
+function getPage(zipcode,res) {
   var url = "https://api.census.gov/data/2017/acs/acs5?get=NAME";
   for (var i = 1; i < 18; i++) {
     url += ",B19001_0";
@@ -21,13 +54,14 @@ app.post('/', function (req, res) {
     }
     url += i + "E";
   }
-  url += "&for=place:53562,state=";
+  url += "&for=zip code tabulation area:"+zipcode;
   request(url, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred and handle it
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     res.send(JSON.stringify(body))
-  })
-});
+  });
+}
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 //census partial query link for delaware:
